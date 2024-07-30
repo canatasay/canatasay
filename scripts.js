a9ae64456d5574805ed6a9b9dfca83199cdb7443
canatasay <s_canatasay@hotmail.com>
2024-07-30 18:03:53 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,29 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var lightbox = document.getElementById('lightbox');
-    var lightboxImage = document.getElementById('lightbox-image');
-    var closeButton = document.querySelector('.close');
-    
-    document.querySelectorAll('.portfolio-link').forEach(function (link) {
-        link.addEventListener('click', function (event) {
+document.addEventListener("DOMContentLoaded", function() {
+    const portfolioLinks = document.querySelectorAll('.portfolio-link');
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    const closeBtn = document.querySelector('.close');
+
+    portfolioLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
             event.preventDefault();
+            const imgSrc = link.getAttribute('href');
+            lightboxImg.setAttribute('src', imgSrc);
             lightbox.style.display = 'block';
-            lightboxImage.src = this.href;
         });
     });
-    
-    closeButton.addEventListener('click', function () {
+
+    closeBtn.addEventListener('click', function() {
         lightbox.style.display = 'none';
     });
-    
-    lightbox.addEventListener('click', function (event) {
-        if (event.target === lightbox) {
+
+    lightbox.addEventListener('click', function(event) {
+        if (event.target !== lightboxImg) {
             lightbox.style.display = 'none';
         }
-    });
-    
-    document.getElementById('language-selector').addEventListener('change', function () {
-        var selectedLanguage = this.value;
-        // Burada dil değiştirme işlemi yapılabilir.
-        console.log('Seçilen dil:', selectedLanguage);
     });
 });
