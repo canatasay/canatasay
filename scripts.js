abacbe2d95df4057fe121ef17922f166c72a8d8b
canatasay <s_canatasay@hotmail.com>
2024-07-30 11:47:49 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var modal = document.getElementById('imageModal');
-    var modalImg = document.getElementById('modalImg');
-    var images = document.querySelectorAll('.portfolio-image');
+document.addEventListener("DOMContentLoaded", function() {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImage = document.getElementById('lightbox-image');
+    const portfolioLinks = document.querySelectorAll('.portfolio-link');
+    const close = document.getElementsByClassName('close')[0];
 
-    images.forEach(function (img) {
-        img.addEventListener('click', function () {
-            modal.style.display = 'block';
-            modalImg.src = this.src;
+    portfolioLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            lightbox.style.display = "block";
+            lightboxImage.src = this.href;
         });
     });
 
-    var span = document.getElementsByClassName('close')[0];
+    close.onclick = function() {
+        lightbox.style.display = "none";
+    }
 
-    span.onclick = function () {
-        modal.style.display = 'none';
+    lightbox.onclick = function(event) {
+        if (event.target === lightbox) {
+            lightbox.style.display = "none";
+        }
     }
 });
