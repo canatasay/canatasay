ac0355be8175999dc4ed7d0d103b4f75fbcb549f
canatasay <s_canatasay@hotmail.com>
2024-07-30 21:54:49 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const portfolioLinks = document.querySelectorAll('.portfolio-link');
-    const lightbox = document.getElementById('lightbox');
-    const lightboxImg = document.getElementById('lightbox-img');
-    const closeBtn = document.querySelector('.close');
+document.addEventListener("DOMContentLoaded", function () {
+    var lightbox = document.getElementById('lightbox');
+    var lightboxImage = document.getElementById('lightbox-image');
+    var closeBtn = document.querySelector('.close');
 
-    portfolioLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
+    document.querySelectorAll('.portfolio-link').forEach(function (link) {
+        link.addEventListener('click', function (event) {
             event.preventDefault();
-            const imgSrc = link.getAttribute('href');
-            lightboxImg.setAttribute('src', imgSrc);
+            lightboxImage.src = this.href;
             lightbox.style.display = 'block';
         });
     });
 
-    closeBtn.addEventListener('click', function() {
+    closeBtn.addEventListener('click', function () {
         lightbox.style.display = 'none';
     });
 
-    lightbox.addEventListener('click', function(event) {
-        if (event.target !== lightboxImg) {
+    lightbox.addEventListener('click', function (event) {
+        if (event.target === lightbox) {
             lightbox.style.display = 'none';
         }
     });
