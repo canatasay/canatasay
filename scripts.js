29ed55055310e87572eba11e356c07eba3eba2b7
canatasay <s_canatasay@hotmail.com>
2024-07-30 12:23:53 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -21,4 +21,22 @@ document.addEventListener("DOMContentLoaded", function() {
             lightbox.style.display = "none";
         }
     }
+
+    // Dil değiştirme işlevi
+    window.changeLanguage = function() {
+        const select = document.getElementById('language-select');
+        const language = select.value;
+
+        if (language === "tr") {
+            window.location.href = "index.html"; // Türkçe ana sayfa URL'si
+        } else if (language === "en") {
+            window.location.href = "index_en.html"; // İngilizce ana sayfa URL'si
+        } else if (language === "es") {
+            window.location.href = "index_es.html"; // İspanyolca ana sayfa URL'si
+        } else if (language === "it") {
+            window.location.href = "index_it.html"; // İtalyanca ana sayfa URL'si
+        } else if (language === "fr") {
+            window.location.href = "index_fr.html"; // Fransızca ana sayfa URL'si
+        }
+    }
 });
