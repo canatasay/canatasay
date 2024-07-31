872241cebdb0dd7131ce7bda9d44823e6a1379fd
canatasay <s_canatasay@hotmail.com>
2024-07-31 16:45:24 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -29,11 +29,24 @@ document.addEventListener("DOMContentLoaded", function() {
         lightbox.classList.remove("active");
     });
 
-    // Dropdown functionality for mobile
+    // Dropdown functionality
     var dropdown = document.querySelectorAll(".dropdown");
     dropdown.forEach(menu => {
-        menu.addEventListener("click", e => {
-            menu.querySelector(".dropdown-content").classList.toggle("show");
+        menu.addEventListener("mouseenter", e => {
+            menu.querySelector(".dropdown-content").classList.add("show");
+        });
+        menu.addEventListener("mouseleave", e => {
+            menu.querySelector(".dropdown-content").classList.remove("show");
         });
     });
+});
+
+// Ensure dropdown-content class 'show' is removed on mobile devices
+window.addEventListener('resize', function() {
+    if (window.innerWidth <= 768) {
+        var dropdownContent = document.querySelectorAll('.dropdown-content');
+        dropdownContent.forEach(menu => {
+            menu.classList.remove('show');
+        });
+    }
 });
