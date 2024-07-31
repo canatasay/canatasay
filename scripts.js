ab3cd83ce38c235c86ad5f16ea70b5d15c1e76b7
canatasay <s_canatasay@hotmail.com>
2024-07-31 21:04:00 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,19 @@
 // Lightbox functionality
 document.addEventListener("DOMContentLoaded", function() {
     var images = document.querySelectorAll(".portfolio-image");
-    var lightbox = document.createElement("div");
-    lightbox.id = "lightbox";
-    document.body.appendChild(lightbox);
+    var lightbox = document.getElementById("lightbox");
+    var lightboxImage = document.getElementById("lightbox-image");
+    var close = document.querySelector(".close");
 
     images.forEach(image => {
         image.addEventListener("click", e => {
             lightbox.classList.add("active");
-            var img = document.createElement("img");
-            img.src = image.src;
-            while (lightbox.firstChild) {
-                lightbox.removeChild(lightbox.firstChild);
-            }
-            lightbox.appendChild(img);
-            var close = document.createElement("span");
-            close.classList.add("close");
-            close.innerHTML = "&times;";
-            lightbox.appendChild(close);
-            close.addEventListener("click", () => {
-                lightbox.classList.remove("active");
-            });
+            lightboxImage.src = image.src;
         });
+    });
+
+    close.addEventListener("click", () => {
+        lightbox.classList.remove("active");
     });
 
     lightbox.addEventListener("click", e => {
